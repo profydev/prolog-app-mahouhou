21b57c1efb7303489e6af66bc1e4e8c2edd92430
mahouhou <freja_smith@hotmail.co.uk>
2023-10-10 14:45:17 +0100

    Further attempt to pass the build test with edits to the setVersion.js script.

--- scripts/setVersion.js
+++ scripts/setVersion.js
@@ -3,8 +3,6 @@ const dotenv = require("dotenv");
 const fs = require("fs");
 const packageJson = require("../package.json");
 
-dotenv.config();
-
 let envConfig;
 
 // Check if the environment variable exists
@@ -13,21 +11,20 @@ if (process.env.NEXT_PUBLIC_APP_VERSION) {
   envConfig = process.env;
 } else {
   // If it doesn't, read the variables from the .env.local file
+  dotenv.config();
   envConfig = dotenv.parse(fs.readFileSync(".env.local"));
 }
 
 // Update version number
 envConfig["NEXT_PUBLIC_APP_VERSION"] = packageJson.version;
 
-// Convert back to string format
-let envConfigStr = "";
-for (let key in envConfig) {
-  if (key === "NEXT_PUBLIC_APP_VERSION") {
-    envConfigStr += `${key}=${envConfig[key]}\n`;
-  }
-}
-
-// Write back to file only if it's not in the GitHub Actions environment
+// Convert back to string format and write back to file only if it's not in the GitHub Actions environment
 if (!process.env.NEXT_PUBLIC_APP_VERSION) {
+  let envConfigStr = "";
+  for (let key in envConfig) {
+    if (key === "NEXT_PUBLIC_APP_VERSION") {
+      envConfigStr += `${key}=${envConfig[key]}\n`;
+    }
+  }
   fs.writeFileSync(".env.local", envConfigStr);
 }
