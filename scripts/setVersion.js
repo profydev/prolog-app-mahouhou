2964f6960710492d31e66cdd37a29ed9fc51d208
mahouhou <freja_smith@hotmail.co.uk>
2023-10-10 14:38:45 +0100

    Amended setVersion.js node script to check whether there is a .env.local file (i.e. local or github build) and act accordingly.

--- scripts/setVersion.js
+++ scripts/setVersion.js
@@ -1,21 +1,33 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const dotenv = require('dotenv');
-const fs = require('fs');
-const packageJson = require('../package.json');
+const dotenv = require("dotenv");
+const fs = require("fs");
+const packageJson = require("../package.json");
 
 dotenv.config();
 
-// Load existing environment variables
-let envConfig = dotenv.parse(fs.readFileSync('.env.local'));
+let envConfig;
 
-// Update version number
-envConfig['NEXT_PUBLIC_APP_VERSION'] = packageJson.version;
-
-// Convert back to string format
-let envConfigStr = '';
-for (let key in envConfig) {
-  envConfigStr += `${key}=${envConfig[key]}\n`;
+// Check if the environment variable exists
+if (process.env.NEXT_PUBLIC_APP_VERSION) {
+  // If it does, use it directly
+  envConfig = process.env;
+} else {
+  // If it doesn't, read the variables from the .env.local file
+  envConfig = dotenv.parse(fs.readFileSync(".env.local"));
 }
 
-// Write back to file
-fs.writeFileSync('.env.local', envConfigStr);
+// Update version number
+envConfig["NEXT_PUBLIC_APP_VERSION"] = packageJson.version;
+
+// Convert back to string format
+let envConfigStr = "";
+for (let key in envConfig) {
+  if (key === "NEXT_PUBLIC_APP_VERSION") {
+    envConfigStr += `${key}=${envConfig[key]}\n`;
+  }
+}
+
+// Write back to file only if it's not in the GitHub Actions environment
+if (!process.env.NEXT_PUBLIC_APP_VERSION) {
+  fs.writeFileSync(".env.local", envConfigStr);
+}
