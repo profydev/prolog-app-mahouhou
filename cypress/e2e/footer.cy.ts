3adb11f75aabf30944e3de40911ef288d8bc36cd
mahouhou <45767941+mahouhou@users.noreply.github.com>
2023-10-11 10:43:45 +0100

    Footer component (#8)
    * Pixel perfect footer component
    
    * Added dynamic version number to footer component. Created a root scripts folder containing node script setVersion.js, an .env.local file, and added a prebuild script to package.json.
    
    * Added Cypress test for footer component.
    
    * Added environment variable NEXT_PUBLIC_APP_VERSION to main.yml and github actions secrets.
    
    * Amended setVersion.js node script to check whether there is a .env.local file (i.e. local or github build) and act accordingly.
    
    * Further attempt to pass the build test with edits to the setVersion.js script.
    
    * Another attempt to pass the build check.
    
    * Added the NEXT_PUBLIC_APP_VERSION variable to the e2e job in main.yml file as previous commit failed e2e tests.
    
    * Added version number environment variable to next.config.js instead, removed .env.local file, setVersion.js script and variable from main.yml. Updated Cypress test to check hard coded version number.
    
    * Forgot to remove setVersion.js from prebuild scripts in package.json

---
+++ cypress/e2e/footer.cy.ts
@@ -0,0 +1,34 @@
+const menuItems = [
+  { text: "Docs", href: "#" },
+  { text: "API", href: "#" },
+  { text: "Help", href: "#" },
+  { text: "Community", href: "#" },
+];
+
+describe("Footer", () => {
+  it("renders correctly", () => {
+    //visit the page
+    cy.visit("http://localhost:3000/dashboard");
+
+    //check for footer element
+    cy.get("footer").should("exist");
+
+    //check for correct number of menu items
+    cy.get("footer").find("li").should("have.length", menuItems.length);
+
+    //check for correct links
+    menuItems.forEach((item) => {
+      cy.get("footer")
+        .contains(item.text)
+        .should("have.attr", "href", item.href);
+    });
+
+    //check for logo
+    cy.get("footer")
+      .find("img")
+      .should("have.attr", "src", "/icons/logo-small.svg");
+
+    //check for version number
+    cy.get("footer").find("span").contains("14.5.2");
+  });
+});
---
+++ features/layout/page-container/footer/footer.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "./footer.module.scss";
+
+const menuItems = [
+  { text: "Docs", href: "#" },
+  { text: "API", href: "#" },
+  { text: "Help", href: "#" },
+  { text: "Community", href: "#" },
+];
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.version}>
+        Version: {process.env.NEXT_PUBLIC_APP_VERSION}
+      </span>
+      <ul className={styles.list}>
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.href} className={styles.menuLink}>
+              {item.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <figure className={styles.logoWrap}>
+        <Image
+          className={styles.logo}
+          src="/icons/logo-small.svg"
+          alt="ProLog"
+          width="23"
+          height="33"
+        />
+      </figure>
+    </footer>
+  );
+}
