87e9c3a1d22df4b139adab08e45ffd0337040fd7
mahouhou <freja_smith@hotmail.co.uk>
2023-10-17 14:35:58 +0100

    Refactored footer Cypress test to include desktop and mobile resolutions.

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -6,29 +6,67 @@ const menuItems = [
 ];
 
 describe("Footer", () => {
-  it("renders correctly", () => {
-    //visit the page
-    cy.visit("http://localhost:3000/dashboard");
+  context("desktop resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1025, 900);
 
-    //check for footer element
-    cy.get("footer").should("exist");
-
-    //check for correct number of menu items
-    cy.get("footer").find("li").should("have.length", menuItems.length);
-
-    //check for correct links
-    menuItems.forEach((item) => {
-      cy.get("footer")
-        .contains(item.text)
-        .should("have.attr", "href", item.href);
+      //visit the dashboard where footer is visible
+      cy.visit("http://localhost:3000/dashboard");
     });
 
-    //check for logo
-    cy.get("footer")
-      .find("img")
-      .should("have.attr", "src", "/icons/logo-small.svg");
+    it("renders correctly", () => {
+      //check for footer element
+      cy.get("footer").should("exist");
 
-    //check for version number
-    cy.get("footer").find("span").contains("14.5.2");
+      //check for correct number of menu items
+      cy.get("footer").find("li").should("have.length", menuItems.length);
+
+      //check for correct links
+      menuItems.forEach((item) => {
+        cy.get("footer")
+          .contains(item.text)
+          .should("have.attr", "href", item.href);
+      });
+
+      //check for logo
+      cy.get("footer")
+        .find("img")
+        .should("have.attr", "src", "/icons/logo-small.svg");
+
+      //check for version number
+      cy.get("footer").find("span").contains("14.5.2");
+    });
+  });
+
+  context("mobile resolution", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-8");
+
+      //visit the dashboard where footer is visible
+      cy.visit("http://localhost:3000/dashboard");
+    });
+
+    it("renders correctly", () => {
+      //check for footer element
+      cy.get("footer").should("exist");
+
+      //check for correct number of menu items
+      cy.get("footer").find("li").should("have.length", menuItems.length);
+
+      //check for correct links
+      menuItems.forEach((item) => {
+        cy.get("footer")
+          .contains(item.text)
+          .should("have.attr", "href", item.href);
+      });
+
+      //check for logo
+      cy.get("footer")
+        .find("img")
+        .should("have.attr", "src", "/icons/logo-small.svg");
+
+      //check for version number
+      cy.get("footer").find("span").contains("14.5.2");
+    });
   });
 });
